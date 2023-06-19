738a8c7d6e5abb39ecc483c578bbd0217f429a8f
Gopal <gopals98877@gmail.com>
2023-06-19 23:45:39 +0530

    This is my 1st Project

---
+++ src/components/data/Data.js
@@ -0,0 +1,319 @@
+export const nav = [
+    {
+      text: "home",
+      path: "/",
+    },
+    {
+      text: "about",
+      path: "/about",
+    },
+    {
+      text: "services",
+      path: "/services",
+    },
+    {
+      text: "blog",
+      path: "/blog",
+    },
+    {
+      text: "pricing",
+      path: "/pricing",
+    },
+    {
+      text: "contact",
+      path: "/contact",
+    },
+  ]
+  export const featured = [
+    {
+      cover: "../images/hero/h1.png",
+      name: "Family House",
+      total: "122 Property",
+    },
+    {
+      cover: "../images/hero/h2.png",
+      name: "House & Villa",
+      total: "155 Property",
+    },
+    {
+      cover: "../images/hero/h3.png",
+      name: "Apartment",
+      total: "300 Property",
+    },
+    {
+      cover: "../images/hero/h4.png",
+      name: "Office & Studio",
+      total: "80 Property",
+    },
+    {
+      cover: "../images/hero/h6.png",
+      name: "Villa & Condo",
+      total: "80 Property",
+    },
+  ]
+  export const list = [
+    {
+      id: 1,
+      cover: "../images/list/p-1.png",
+      name: "Red Carpet Real Estate",
+      location: "Amritsar, Punjab",
+      category: "For Rent",
+      price: "₹3,700",
+      type: "Apartment",
+    },
+    {
+      id: 2,
+      cover: "../images/list/p-2.png",
+      name: "Fairmount Properties",
+      location: "Delhi, India",
+      category: "For Sale",
+      price: "₹9,750",
+      type: "Condos",
+    },
+    {
+      id: 3,
+      cover: "../images/list/p-7.png",
+      name: "The Real Estate Corner",
+      location: "Mumbai, Maharashtra",
+      category: "For Rent",
+      price: "₹5,860",
+      type: "Offices",
+    },
+    {
+      id: 4,
+      cover: "../images/list/p-4.png",
+      name: "Herringbone Realty",
+      location: "Aligarh, Uttar Pradesh",
+      category: "For Rent",
+      price: "₹7,540",
+      type: "Homes & Villas",
+    },
+    {
+      id: 5,
+      cover: "../images/list/p-5.png",
+      name: "Brick Lane Realty",
+      location: "Jalandhar, Punjab",
+      category: "For Rent",
+      price: "₹4,850",
+      type: "Commercial",
+    },
+    {
+      id: 6,
+      cover: "../images/list/p-6.png",
+      name: "Banyon Tree Realty",
+      location: "Delhi, India",
+      category: "For Sale",
+      price: "₹2,742",
+      type: "Apartment",
+    },
+  ]
+  export const awards = [
+    {
+      icon: <i class='fa-solid fa-trophy'></i>,
+      num: "32 M	",
+      name: "Blue Burmin Award",
+    },
+    {
+      icon: <i class='fa-solid fa-briefcase'></i>,
+      num: "43 M",
+      name: "Mimo X11 Award",
+    },
+    {
+      icon: <i class='fa-solid fa-lightbulb'></i>,
+      num: "51 M",
+      name: "Australian UGC Award",
+    },
+    {
+      icon: <i class='fa-solid fa-heart'></i>,
+      num: "42 M",
+      name: "IITCA Green Award",
+    },
+  ]
+  export const location = [
+    {
+      id: 1,
+      name: "Amritsar, Punjab",
+      Villas: "12 Villas",
+      Apartments: "10 Apartments",
+      Offices: "07 Offices",
+      cover: "./images/location/city-1.png",
+    },
+    {
+      id: 2,
+      name: "Jaipur, Rajasthan",
+      Villas: "12 Villas",
+      Apartments: "10 Apartments",
+      Offices: "07 Offices",
+      cover: "./images/location/city-2.png",
+    },
+    {
+      id: 3,
+      name: "Kolkata, West Bengal",
+      Villas: "12 Villas",
+      Apartments: " 10 Apartments",
+      Offices: "07 Offices",
+      cover: "./images/location/city-3.png",
+    },
+    {
+      id: 4,
+      name: "Mumbai, Maharashtra",
+      Villas: "12 Villas",
+      Apartments: " 10 Apartments",
+      Offices: "07 Offices",
+      cover: "./images/location/city-4.png",
+    },
+    {
+      id: 5,
+      name: "Delhi, India",
+      Villas: "12 Villas",
+      Apartments: " 10 Apartments",
+      Offices: "07 Offices",
+      cover: "./images/location/city-5.png",
+    },
+    {
+      id: 6,
+      name: "Bengaluru, Karnataka",
+      Villas: "12 Villas",
+      Apartments: " 10 Apartments",
+      Offices: "07 Offices",
+      cover: "./images/location/city-6.png",
+    },
+  ]
+  export const team = [
+    {
+      list: "50",
+      cover: "../images/customer/team-1.jpg",
+      address: "Uttar Pradesh, India",
+      name: "Anubhav Awasti",
+      icon: [<i class='fa-brands fa-facebook-f'></i>, <i class='fa-brands fa-linkedin'></i>, <i class='fa-brands fa-twitter'></i>, <i class='fa-brands fa-instagram'></i>],
+    },
+    {
+      list: "70",
+      cover: "../images/customer/team-2.jpg",
+      address: "Uttar Pradesh, India",
+      name: "Rishabh Rai",
+      icon: [<i class='fa-brands fa-facebook-f'></i>, <i class='fa-brands fa-linkedin'></i>, <i class='fa-brands fa-twitter'></i>, <i class='fa-brands fa-instagram'></i>],
+    },
+    {
+      list: "80",
+      cover: "../images/customer/team-3.jpg",
+      address: "Bihar, India",
+      name: "Raushan Kumar",
+      icon: [<i class='fa-brands fa-facebook-f'></i>, <i class='fa-brands fa-linkedin'></i>, <i class='fa-brands fa-twitter'></i>, <i class='fa-brands fa-instagram'></i>],
+    },
+    {
+      list: "51",
+      cover: "../images/customer/team-4.jpg",
+      address: "Uttar Pradesh, India",
+      name: "Yuvraj Singh",
+      icon: [<i class='fa-brands fa-facebook-f'></i>, <i class='fa-brands fa-linkedin'></i>, <i class='fa-brands fa-twitter'></i>, <i class='fa-brands fa-instagram'></i>],
+    },
+    {
+      list: "42",
+      cover: "../images/customer/team-5.jpg",
+      address: "Uttar Pradesh, India",
+      name: "Gopal Singh",
+      icon: [<i class='fa-brands fa-facebook-f'></i>, <i class='fa-brands fa-linkedin'></i>, <i class='fa-brands fa-twitter'></i>, <i class='fa-brands fa-instagram'></i>],
+    },
+    {
+      list: "38",
+      cover: "../images/customer/team-6.jpg",
+      address: "Haryana, India",
+      name: "Vikash Verma",
+      icon: [<i class='fa-brands fa-facebook-f'></i>, <i class='fa-brands fa-linkedin'></i>, <i class='fa-brands fa-twitter'></i>, <i class='fa-brands fa-instagram'></i>],
+    },
+  ]
+  export const price = [
+    {
+      plan: "Basic",
+      price: "75",
+      ptext: "per user, per month",
+      list: [
+        {
+          icon: <i class='fa-solid fa-check'></i>,
+          text: "99.5% Uptime Guarantee",
+        },
+        {
+          icon: <i class='fa-solid fa-check'></i>,
+          text: "10 Property Listings",
+        },
+        {
+          icon: <i class='fa-solid fa-check'></i>,
+          text: "Tenant Screening",
+        },
+        { change: "color", icon: <i class='fa-solid fa-x'></i>, text: "Personal Help Support" },
+        { change: "color", icon: <i class='fa-solid fa-x'></i>, text: "Contact and Communication with Tanent" },
+      ],
+    },
+    {
+      best: "Best Value",
+      plan: "Standard",
+      price: "120",
+      ptext: "per user, per month",
+      list: [
+        {
+          icon: <i class='fa-solid fa-check'></i>,
+          text: "99.5% Uptime Guarantee",
+        },
+        {
+          icon: <i class='fa-solid fa-check'></i>,
+          text: "15 Property Listings",
+        },
+        {
+          icon: <i class='fa-solid fa-check'></i>,
+          text: "Tenant Screening",
+        },
+        {
+          icon: <i class='fa-solid fa-check'></i>,
+          text: "Personal Help Support",
+        },
+        {
+          change: "color",
+          icon: <i class='fa-solid fa-x'></i>,
+          text: "Contact and Communication with Tanent",
+        },
+      ],
+    },
+    {
+      plan: "Platinum",
+      price: "160",
+      ptext: "2 user, per month",
+      list: [
+        {
+          icon: <i class='fa-solid fa-check'></i>,
+          text: "100% Uptime Guarantee",
+        },
+        {
+          icon: <i class='fa-solid fa-check'></i>,
+          text: "25 Property Listings",
+        },
+        {
+          icon: <i class='fa-solid fa-check'></i>,
+          text: "Tenant Screening",
+        },
+        {
+          icon: <i class='fa-solid fa-check'></i>,
+          text: "Personal Help Support",
+        },
+        {
+          icon: <i class='fa-solid fa-check'></i>,
+          text: "Contact and Communication with Tanent",
+        },
+      ],
+    },
+  ]
+  export const footer = [
+    {
+      title: "LAYOUTS",
+      text: [{ list: "Home Page" }, { list: "About Page" }, { list: "Service Page" }, { list: "Property Page" }, { list: "Contact Page" }, { list: "Single Blog" }],
+    },
+    {
+      title: "ALL SECTIONS",
+      text: [{ list: "Headers" }, { list: "Features" }, { list: "Attractive" }, { list: "Testimonials" }, { list: "Videos" }, { list: "Footers" }],
+    },
+    {
+      title: "COMPANY",
+      text: [{ list: "About" }, { list: "Blog" }, { list: "Pricing" }, { list: "Affiliate" }, { list: "Login" }, { list: "Changelog" }],
+    },
+  ]
+  
