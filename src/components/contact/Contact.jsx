c6d0418e45139a8bb78299e4c1200d8401a2ce3a
Gopal <gopals98877@gmail.com>
2023-09-12 11:09:05 +0530

    project

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -9,14 +9,15 @@ const Contact = () => {
       <section className='contact mb'>
         <Back name='Contact Us' title='Get Helps & Friendly Support' cover={img} />
         <div className='container'>
-          <form className='shadow'>
+        <form action="https://formspree.io/f/xdorgyzg" className='shadow' method="POST">
             <h4>Fillup The Form</h4> <br />
             <div>
-              <input type='text' placeholder='Name' />
-              <input type='text' placeholder='Email' />
+
+              <input type='text' placeholder='Name' name="Name" required autoComplete="off" />
+              <input type='text' placeholder='Email' name="Email" required autoComplete="off" />
             </div>
-            <input type='text' placeholder='Subject' />
-            <textarea cols='30' rows='10'></textarea>
+            <input type='text' placeholder='Subject' name="Subject" required autoComplete="off" />
+            <textarea cols='30' rows='10' name="Message" required autoComplete="off" ></textarea>
             <button>Submit Request</button>
           </form>
         </div>
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,5 +1,4 @@
 import React from "react"
-import Awards from "./awards/Awards"
 import Featured from "./featured/Featured"
 import Hero from "./hero/Hero"
 import Location from "./location/Location"
@@ -13,7 +12,6 @@ const Home = () => {
       <Hero />
       <Featured />
       <Recent />
-      <Awards />
       <Location />
       <Team />
       <Price />
