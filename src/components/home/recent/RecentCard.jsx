53c82727dbfaf5d346f0dacd374a082933fb2be0
ThakurGopal27 <gopals98877@gmail.com>
2023-12-30 23:42:46 +0530

    re-commit

--- src/components/home/recent/RecentCard.jsx
+++ src/components/home/recent/RecentCard.jsx
@@ -1,7 +1,10 @@
 import React from "react"
 import { list } from "../../data/Data"
 
+
 const RecentCard = () => {
+
+
   return (
     <>
       <div className='content grid3 mtop'>
@@ -15,7 +18,8 @@ const RecentCard = () => {
               <div className='text'>
                 <div className='category flex'>
                   <span style={{ background: category === "For Sale" ? "#25b5791a" : "#ff98001a", color: category === "For Sale" ? "#25b579" : "#ff9800" }}>{category}</span>
-                  <i className='fa fa-heart'></i>
+                  <i  className='fa fa-heart'></i>
+                 
                 </div>
                 <h4>{name}</h4>
                 <p>
